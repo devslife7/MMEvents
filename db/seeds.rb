204fe58f367b25df896fbfa0af71b12858a26788
Marcos Velasco <devslife7@gmail.com>
2020-07-09 17:16:28 -0400

    fixed user show table

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,17 +42,18 @@ Event.create(location_id: Location.find_by(city: "Las Vegas, NV").id, name: "NFL
 Event.create(location_id: Location.find_by(city: "Los Angeles, CA").id, name: "E3 Expo", type_of_event: "Convention", ticket_cost: 250, date: Date.parse('2021-06-15'), img: "E3_logo.png")
 Event.create(location_id: Location.find_by(city: "Atlanta, GA").id, name: "MLB All-Star Game", type_of_event: "Sports", ticket_cost: 1500, date: Date.parse('2021-07-13'), img: "All_Star_Game_logo.png")
 
-10.times do
-    User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: Faker::Esport.unique.player, password_digest: Faker::Hipster.word)
-end
+# 10.times do
+#     User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: Faker::Esport.unique.player, password_digest: Faker::Hipster.word)
+# end
+User.create(first_name: "Marcos", last_name: "Velasco", username: "mk", password: "mk", password_confirmation: "mk")
 User.create(first_name: "Marcos", last_name: "Velasco", username: "Mark", password: "password", password_confirmation: "password")
 User.create(first_name: "Mike", last_name: "Causey", username: "Mike", password: "password", password_confirmation: "password")
 
 
-50.times do 
-    EventUser.create(user_id: User.all.sample.id, event_id: Event.all.sample.id)
-end
+# 50.times do 
+#     EventUser.create(user_id: User.all.sample.id, event_id: Event.all.sample.id)
+# end
 
-50.times do
-    HotelUser.create(user_id: User.all.sample.id, hotel_id: Hotel.all.sample.id, length_of_stay: rand(1..4))
-end
+# 50.times do
+#     HotelUser.create(user_id: User.all.sample.id, hotel_id: Hotel.all.sample.id, length_of_stay: rand(1..4))
+# end
