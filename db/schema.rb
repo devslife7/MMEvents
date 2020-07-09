0c983688ef556ddf9142f5e2ad5115b65fda36e2
Marcos Velasco <devslife7@gmail.com>
2020-07-09 15:53:53 -0400

    added img column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_181840) do
+ActiveRecord::Schema.define(version: 2020_07_09_194328) do
 
   create_table "event_users", force: :cascade do |t|
     t.integer "user_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2020_07_06_181840) do
     t.datetime "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "img"
   end
 
   create_table "hotel_users", force: :cascade do |t|
@@ -56,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_07_06_181840) do
     t.string "last_name"
     t.string "username"
     t.string "password_digest"
+    t.integer "budget"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
